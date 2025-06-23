fd694db2d9d331eaefbc30e9c9ba5c990b501b31
Zahit <zahidjklmn@gmail.com>
2025-06-23 15:03:14 +0300

    seo denemeleri

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   siteUrl: 'https://pltyapitokat.com',
   generateRobotsTxt: true,
+  sourceDir: '.next',
   exclude: [
     '/app/debug',
     '/app/api',
@@ -19,5 +20,13 @@ module.exports = {
         ]
       }
     ]
+  },
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: 'monthly',
+      priority: 0.7,
+      lastmod: new Date().toISOString(),
+    }
   }
-};
+}
